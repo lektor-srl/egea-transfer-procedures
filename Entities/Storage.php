808523fa519fbd30806cfe287817ee077a8e7dc8
nbutturalektor <n.buttura@lektor.it>
2023-04-17 11:50:14 +0200

    refactoring

--- Entities/Storage.php
+++ Entities/Storage.php
@@ -190,7 +190,6 @@ class Storage extends StorageClient{
     {
         try {
 
-            //test
             // Il file ha il seguente formato: progressivo_sequenza_tipo_dataora.jpg
             $record = null;
             $strings = explode('/', $filePath);
