b5eb8cef8f350e906af757bb28b0f12990fe5e6c
nbutturalektor <n.buttura@lektor.it>
2023-05-25 14:59:18 +0200

    Messa a regime la procedura delle foto

--- Jobs/attachmentsProcedure.php
+++ Jobs/attachmentsProcedure.php
@@ -6,11 +6,14 @@ $args = getopt("", ["mode:", "from:", "to:", "limit:"]);
 
 if(!$args || $args == ''){
     echo "Arguments missing";
+    die();
 }
-// Se non vengono passati parametri nelle date, prende la data corrente
+// Se non vengono passati parametri nelle date, prende gli ultimi 2 mesi
+$from = new \DateTime();
+$from->sub(new \DateInterval('P2M'));
 $params = [
     'limit' => $args['limit'] ?? null,                     // provide a limit to the attachment downloaded
-    'dateFrom' => $args['from'] ?? date('Y-m-d'),   // must be in this format: YYYY-mm-dd
+    'dateFrom' => $args['from'] ?? $from->format('Y-m-d'),   // must be in this format: YYYY-mm-dd
     'dateTo' => $args['to'] ?? date('Y-m-d'),       // must be in this format: YYYY-mm-dd
 ];
 
